7a6fdfcefbd2fe0ee39a8b0d5a0aaf7d9b8fa6d2
Martin Petroski <martinpetroski@gmail.com>
2025-02-04 19:18:31 +0100

    tic-tac-toe

---
+++ src/components/Gameboard.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function Gameboard({ onsSelectSquare, board }) {
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onsSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
